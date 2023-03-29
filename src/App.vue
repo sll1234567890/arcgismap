0110bd7271ecc738d7e198fc9afb4b399c9bae02
苏靓靓 <822658702@qq.com>
2023-03-29 17:26:19 +0800

    引入element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <span>ssss</span>
+    <span>ssss4444444444</span>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
   </div>
 </template>
 
@@ -14,7 +16,9 @@ export default {
 </script>
 
 <style>
-html,body,#app{
+html,
+body,
+#app {
   position: relative;
   width: 100%;
   height: 100%;
